742c262d1769806a269769b2d344872cbd1969de
Simmy Kumari <112470623+SimmyKumari12@users.noreply.github.com>
2026-04-24 17:31:38 +0530

    Time: 16 ms (5.67%), Space: 46.8 MB (8.14%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -2,11 +2,9 @@ class Solution {
     public boolean isPalindrome(int x) {
         String s = String.valueOf(x);
         String ans = "";
-
         for(int i = s.length() - 1; i >= 0; i--){
             ans = ans + s.charAt(i);
         }
-
         return s.equals(ans);
     }
 }
