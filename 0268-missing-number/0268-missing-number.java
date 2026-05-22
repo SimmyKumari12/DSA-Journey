87b4d322c96ec251958db09d412a4f8126e39475
Simmy Kumari <112470623+SimmyKumari12@users.noreply.github.com>
2026-05-22 13:54:49 +0530

    Time: 0 ms (100%), Space: 47.6 MB (19.49%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,18 +1,14 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int n = nums.length;
-        int freq[] = new int[n + 1];
-        int ans = 0;
-
+        int xor = 0;
+        
         for(int i = 0; i < n; i++){
-            freq[nums[i]]++;
+            xor ^= nums[i];
+            xor ^= i;
         }
+        xor = xor ^ n;
 
-        for(int i = 0; i < freq.length; i++){
-            if(freq[i] == 0){
-                ans = i;
-            }
-        }
-        return ans;
+        return xor;
     }
 }
