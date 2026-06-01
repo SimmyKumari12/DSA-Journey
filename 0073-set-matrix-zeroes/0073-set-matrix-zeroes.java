c4a801cf5dbc23f7a6e04b8f88c76ae306a7f022
Simmy Kumari <112470623+SimmyKumari12@users.noreply.github.com>
2026-06-01 13:33:31 +0530

    Time: 1 ms (98.29%), Space: 47.8 MB (12.56%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -3,24 +3,53 @@ class Solution {
         int n = matrix.length;
         int m = matrix[0].length;
 
-        boolean[] rows = new boolean[n];
-        boolean[] cols = new boolean[m];
+        boolean firstRowZero = false;
+        boolean firstColZero = false;
 
+        //Check for Row
+        for(int j = 0; j < m; j++){
+            if(matrix[0][j] == 0){
+                firstRowZero = true;
+                break;
+            }
+        }
+
+        //Check for Col
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
+            if(matrix[i][0] == 0){
+                firstColZero = true;
+                break;
+            }
+        }
+
+        //Check for zeroes excluding firstrow and firstcolumn
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < m; j++){
                 if(matrix[i][j] == 0){
-                    rows[i] = true;
-                    cols[j] = true;
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
 
-        //Filling values
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(rows[i] || cols[j]){
+        //Updating the rows and columns according to firstrow and firstcolumn
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < m; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
                     matrix[i][j] = 0;
                 }
+            }
+        }
+
+        if(firstRowZero){
+            for(int j = 0; j < m; j++){
+                matrix[0][j] = 0;
+            }
+        }
+
+        if(firstColZero){
+            for(int i = 0; i < n; i++){
+                matrix[i][0] = 0;
             }
         }
     }
